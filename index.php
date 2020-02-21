fef8b981954c1e3943a81a16439b7aa5f8f520d1
Sergey Aleinikov <chaositomail@gmail.com>
2020-02-21 14:54:22 +0600

    Added tasks 1-6

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
 require('src/functions.php');
+
+# Домашнее задание №2
+// Задание №1
+task1(['Каждый', 'элемент', 'масссива']);
+print task1(['в', 'теге', htmlspecialchars('<p>')], 1);
+print '<hr>';
+
+// Задание №2
+print task2('-', 2, 3, 6);
+print '<hr>';
+
+// Задание №3
+task3(5, 6);
+print '<hr>';
+
+// Задание №4
+task4();
+print '<hr>';
+
+// Задание №5
+task5();
+print '<hr>';
+
+// Задание №6
+$fileName = 'test.txt';
+file_put_contents($fileName, 'Hello again!');
+task6($fileName);
+print '<hr>';
+
--- src/functions.php
+++ src/functions.php
@@ -1,41 +1,67 @@
 <?php
 
-function task1(){
+# Домашнее задание №2
+// Задание №1
+function task1($strArr, $ret = false)
+{
+    $str = implode('', array_map(function($el){return "<p>{$el}</p>";}, $strArr));
+
+    if ($ret) {
+        return $str;
+    } else {
+        print $str;
+    }
+}
+
+// Задание №2
+function task2($mathAction, ...$numbers)
+{
+    $res = 1;
+    switch ($mathAction) {
+        case '+': $res = 0; foreach($numbers AS $value) $res += $value; break;
+        case '-': $res = 0; foreach($numbers AS $value) $res -= $value; break;
+        case '*': foreach($numbers AS $value) $res *= $value; break;
+        case '/': foreach($numbers AS $value) $res /= $value; break;
+        case '%': foreach($numbers AS $value) $res %= $value; break;
+    }
+    return $res;
+}
+
+// Задание №3
+function task3($num1, $num2)
+{
+    if (!is_int($num1) || !is_int($num2) || (int)$num1 < 1 || (int)$num2 < 1) {
+        throw new Exception('Неверные входные данные, оба аргументы должны быть положительными целыми числами больше 0!');
+    }
+
+    print '<table cellpadding="5" cellspacing="0" border="1">';
+    for ($i = 1; $i <= $num1; $i++) {
+        print '<tr>';
+        for ($j = 1; $j <= $num2; $j++) {
+            print "<td style='text-align:center'>".($i * $j)."</td>";
+        }
+        print '</tr>';
+    }
+    print '</table>';
+}
+
+function task4()
+{
+    print date('d.m.Y H:i').'<br>';
+    print strtotime('24.02.2016 00:00:00').'<br>';
+}
+
+function task5()
+{
+    $firstString = "Карл у Клары украл Кораллы";
+    $secondString = "Две бутылки лимонада";
+
+    print $firstString.' => '.str_replace('К','', $firstString).'<br>';
+    print $secondString.' => '.str_replace('Две', 'Три', $secondString).'<br>';
 
 }
 
-function task2(){
-
-}
-
-function task3(){
-
-}
-
-function task4(){
-
-}
-
-function task5(){
-
-}
-
-function task6(){
-
-}
-
-function task7(){
-
-}
-
-function task8(){
-
-}
-
-function task9(){
-
-}
-
-function task10(){
-
+function task6($fileForOpen)
+{
+    print file_get_contents($fileForOpen);
 }
